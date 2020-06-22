8d2e1a24aa39aa87a820bc8e521150b724ee608f
Jinwei Sun <>
2020-06-22 15:23:17 +0800

    初始化项目

---
+++ rocketmq-service/src/test/java/com/xiao/service/RocketmqServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiao.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketmqServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
